6ed04d8ef603f307d3bc62847bfc92fc9042194b
Christoph Burschka <christoph@burschka.de>
2015-10-09 12:09:22 +0200

    Functions for searching the block list by (internal) name and printing the result.

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from nbt import nbt
 import sys
 
-def blockIDs(worldDir):
+def loadBlockData(worldDir):
   x = nbt.NBTFile(worldDir + '/level.dat')
   return {
 	y['V'].value : y['K'].value[1:]
 	for y in x['FML']['ItemData']
 	if y['K'].value[0] == "\x01"
   }
-    
-print(blockIDs(sys.argv[1]))
+
+def filterBlocks(blockData, keyword):
+  return {a:b for a,b in blockData.items() if keyword in b}
+
+def printBlockData(blockData):
+  return "\n".join("{:5}\t{}".format(a,b) for a,b in blockData.items())
+
+x = loadBlockData(sys.argv[1])
+print(printBlockData(filterBlocks(x, sys.argv[2])))
